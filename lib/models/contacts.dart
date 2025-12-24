8303923ebdea0ee22659b379730e4317a7d9c31d
DM <944095635@qq.com>
2025-12-24 16:05:30 +0800

    使用最新SDK搭建

--- lib/models/contacts.dart
+++
@@ -1,8 +0,0 @@
-class Contacts {
-  //显示名
-  late String name;
-  //显示头像
-  late String portrait;
-  //是否在线
-  bool online = false;
-}
--- lib/utils/keyboard_utils.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/material.dart';
-
-class KeyboardUtils {
-  static void hide() => FocusManager.instance.primaryFocus?.unfocus();
-}
